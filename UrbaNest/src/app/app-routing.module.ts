f7123d4dcd10778d2601eb33ebdbb7b7d69f9762
Jebas Winston E <jebaswinston55@gmail.com>
2023-04-05 18:42:29 +0530

    Added Header Component

--- UrbaNest/src/app/app-routing.module.ts
+++ UrbaNest/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
